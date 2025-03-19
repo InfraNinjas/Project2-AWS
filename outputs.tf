1f1f5d88e2ddbc64eaed596b3cfb0592621c83d0
LeverUp-Dev <ododo3@naver.com>
2025-03-19 20:03:56 +0900

    료이키 텐카이3

--- outputs.tf
+++ outputs.tf
@@ -1,22 +1,22 @@
-# output "cluster_name" {
-#   description = "Name of EKS Cluster"
-#   value       = module.eks.cluster_name
-# }
+output "cluster_name" {
+  description = "Name of EKS Cluster"
+  value       = module.eks.cluster_name
+}
 
-# output "tunnel1_preshared_key" {
-#   value = nonsensitive(aws_vpn_connection.aws_on_premise.tunnel1_preshared_key)
-# }
+output "tunnel1_preshared_key" {
+  value = nonsensitive(aws_vpn_connection.aws_on_premise.tunnel1_preshared_key)
+}
 
-# output "tunnel1_address" {
-#   value = aws_vpn_connection.aws_on_premise.tunnel1_address
-# }
+output "tunnel1_address" {
+  value = aws_vpn_connection.aws_on_premise.tunnel1_address
+}
 
 #
 # RDS
 #
-# output "db_address" {
-#   value = aws_db_instance.myDB.address
-# }
+output "db_address" {
+  value = aws_db_instance.myDB.address
+}
 
 #
 # WAF
